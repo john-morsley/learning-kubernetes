72c32402d39ecab38c4d25e73ae728807c658f99
John Morsley <johnmorsley@gmail.com>
2021-03-20 11:00:07 +0000

    Added very minimal example API

---
+++ src/ExampleApi/Controllers/EnvironmentController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ExampleApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EnvironmentController : ControllerBase
+    {
+        private readonly ILogger<EnvironmentController> _logger;
+
+        public EnvironmentController(ILogger<EnvironmentController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("/environment")]
+        public string Get()
+        {
+            return Environment.MachineName;
+        }
+    }
+}
